0aa3f8d9207245fb7c6f2616c06518cdaa281267
Salim Ramirez <salimramirezm@gmail.com>
2025-08-07 02:44:16 -0500

    chore: initial commit

---
+++ src/main/java/com/demy/platform/DemyTestApplication.java
@@ -0,0 +1,13 @@
+package com.demy.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemyTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemyTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demy/platform/DemyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demy.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemyTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
