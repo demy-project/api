d75f58beb55eab99c9b8ce34bb3484b976ae07b1
Salim Ramirez <salimramirezm@gmail.com>
2025-08-09 22:50:12 -0500

    feat(institution): add Academy aggregate root with embedded AdministratorId and Ruc

---
+++ src/main/java/com/demy/platform/institution/domain/model/aggregates/Academy.java
@@ -0,0 +1,28 @@
+package com.demy.platform.institution.domain.model.aggregates;
+
+import com.demy.platform.institution.domain.model.valueobjects.AdministratorId;
+import com.demy.platform.institution.domain.model.valueobjects.Ruc;
+import com.demy.platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+
+@Entity
+public class Academy extends AuditableAbstractAggregateRoot<Academy> {
+
+    @Getter
+    @Embedded
+    private AdministratorId administratorId;
+
+    @Getter
+    @Embedded
+    private Ruc ruc;
+
+    /**
+     * Default constructor for JPA
+     */
+    public Academy() {
+        this.administratorId = new AdministratorId();
+        this.ruc = new Ruc();
+    }
+}
