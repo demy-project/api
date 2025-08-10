85363a0093dcc3d8470f0ef81262da835488a0d9
Salim Ramirez <salimramirezm@gmail.com>
2025-08-10 16:07:59 -0500

    docs(institution): add Javadoc comments to AcademyResource for clarity

--- src/main/java/com/demy/platform/institution/interfaces/rest/resources/AcademyResource.java
+++ src/main/java/com/demy/platform/institution/interfaces/rest/resources/AcademyResource.java
@@ -1,5 +1,17 @@
 package com.demy.platform.institution.interfaces.rest.resources;
 
+/**
+ * Represents a resource for an Academy in the REST API.
+ * <p>
+ * This record is used to transfer data related to an Academy.
+ *
+ * @param id the unique identifier of the academy
+ * @param administratorId the ID of the administrator managing the academy
+ * @param ruc the Registro Único de Contribuyentes (RUC) of the academy
+ *
+ * @author Salim Ramirez
+ * @since 1.0.0
+ */
 public record AcademyResource(
         Long id,
         String administratorId,
