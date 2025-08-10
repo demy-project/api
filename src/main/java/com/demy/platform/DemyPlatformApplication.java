b735ae7b23278e324bcb80f177efaaf0bba15ce9
Salim Ramirez <salimramirezm@gmail.com>
2025-08-09 20:51:34 -0500

    refactor: rename application classes to align with platform naming

--- src/main/java/com/demy/platform/DemyPlatformApplication.java
+++ src/main/java/com/demy/platform/DemyPlatformApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemyApiApplication {
+public class DemyPlatformApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemyApiApplication.class, args);
+        SpringApplication.run(DemyPlatformApplication.class, args);
     }
 
 }
