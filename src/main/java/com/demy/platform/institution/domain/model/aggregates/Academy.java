6353e9c22282ff25168a0ec5c04903c626e0a015
Salim Ramirez <salimramirezm@gmail.com>
2025-08-10 04:07:44 -0500

    feat(institution): add constructors to Academy for initialization with AdministratorId and Ruc

--- src/main/java/com/demy/platform/institution/domain/model/aggregates/Academy.java
+++ src/main/java/com/demy/platform/institution/domain/model/aggregates/Academy.java
@@ -1,5 +1,6 @@
 package com.demy.platform.institution.domain.model.aggregates;
 
+import com.demy.platform.institution.domain.model.commands.RegisterAcademyCommand;
 import com.demy.platform.institution.domain.model.valueobjects.AdministratorId;
 import com.demy.platform.institution.domain.model.valueobjects.Ruc;
 import com.demy.platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
@@ -22,4 +23,13 @@ public class Academy extends AuditableAbstractAggregateRoot<Academy> {
      * Default constructor for JPA
      */
     protected Academy() {}
+
+    public Academy(AdministratorId administratorId, Ruc ruc) {
+        this.administratorId = administratorId;
+        this.ruc = ruc;
+    }
+
+    public Academy(RegisterAcademyCommand command) {
+        this(command.administratorId(), command.ruc());
+    }
 }
