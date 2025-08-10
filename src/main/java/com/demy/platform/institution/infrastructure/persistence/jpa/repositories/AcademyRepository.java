f2cc78dfdb30974ca9930e437716a1a69369b86c
Salim Ramirez <salimramirezm@gmail.com>
2025-08-10 03:45:17 -0500

    feat(institution): add existsByRuc method to AcademyRepository for RUC existence check

--- src/main/java/com/demy/platform/institution/infrastructure/persistence/jpa/repositories/AcademyRepository.java
+++ src/main/java/com/demy/platform/institution/infrastructure/persistence/jpa/repositories/AcademyRepository.java
@@ -1,20 +1,22 @@
 package com.demy.platform.institution.infrastructure.persistence.jpa.repositories;
 
 import com.demy.platform.institution.domain.model.aggregates.Academy;
+import com.demy.platform.institution.domain.model.valueobjects.Ruc;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
  * Repository for managing Academy entities.
- *
  * <p>
  * Extends JpaRepository to provide standard CRUD operations for the Academy aggregate
  * and allows for custom queries if needed.
- * </p>
  *
+ * @author Salim Ramirez
+ * @see Academy
  * @since 1.0.0
  */
 @Repository
 public interface AcademyRepository extends JpaRepository<Academy, Long> {
 
+    boolean existsByRuc(Ruc ruc);
 }
