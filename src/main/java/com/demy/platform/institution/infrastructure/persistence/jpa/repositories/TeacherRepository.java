f5b2184b168d3430177bfc119341b47630a02a26
Salim Ramirez <salimramirezm@gmail.com>
2025-08-10 02:25:40 -0500

    feat(institution): add method to find Teachers by AcademyId in TeacherRepository

--- src/main/java/com/demy/platform/institution/infrastructure/persistence/jpa/repositories/TeacherRepository.java
+++ src/main/java/com/demy/platform/institution/infrastructure/persistence/jpa/repositories/TeacherRepository.java
@@ -1,10 +1,14 @@
 package com.demy.platform.institution.infrastructure.persistence.jpa.repositories;
 
 import com.demy.platform.institution.domain.model.aggregates.Teacher;
+import com.demy.platform.shared.domain.model.valueobjects.AcademyId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
 
+    List<Teacher> findAllByAcademyId(AcademyId academyId);
 }
