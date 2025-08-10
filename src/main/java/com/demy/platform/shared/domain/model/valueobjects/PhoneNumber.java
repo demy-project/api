cad3e3d62f681c185ccb7ed52637ba4befe1a746
Salim Ramirez <salimramirezm@gmail.com>
2025-08-10 17:59:31 -0500

    feat(shared): add PhoneNumber default constructor

--- src/main/java/com/demy/platform/shared/domain/model/valueobjects/PhoneNumber.java
+++ src/main/java/com/demy/platform/shared/domain/model/valueobjects/PhoneNumber.java
@@ -1,10 +1,17 @@
 package com.demy.platform.shared.domain.model.valueobjects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 
 @Embeddable
 public record PhoneNumber(
+        @Column(nullable = false, length = 5)
         String countryCode,
+
+        @Column(nullable = false, length = 15)
         String number
 ) {
+    public PhoneNumber() {
+        this("", "");
+    }
 }
