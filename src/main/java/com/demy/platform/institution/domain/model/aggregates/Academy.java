2bbb88335df30ed633b51810b8dd24fc5c9c042b
Salim Ramirez <salimramirezm@gmail.com>
2025-08-10 04:12:22 -0500

    docs(institution): improve Academy documentation for better legibility

--- src/main/java/com/demy/platform/institution/domain/model/aggregates/Academy.java
+++ src/main/java/com/demy/platform/institution/domain/model/aggregates/Academy.java
@@ -8,6 +8,18 @@ import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 
+/**
+ * Represents an Academy entity in the domain model.
+ * <p>
+ * This class extends {@link AuditableAbstractAggregateRoot} to provide auditing capabilities.
+ * It contains the administrator ID and RUC (Registro Único de Contribuyentes) as embedded value objects.
+ *
+ * @author Salim Ramirez
+ * @see AuditableAbstractAggregateRoot
+ * @see AdministratorId
+ * @see Ruc
+ * @since 1.0.0
+ */
 @Entity
 public class Academy extends AuditableAbstractAggregateRoot<Academy> {
 
@@ -24,11 +36,25 @@ public class Academy extends AuditableAbstractAggregateRoot<Academy> {
      */
     protected Academy() {}
 
+    /**
+     * Constructs an Academy instance with the specified administrator ID and RUC.
+     *
+     * @param administratorId the ID of the administrator
+     * @param ruc the RUC of the academy
+     * @see AdministratorId
+     * @see Ruc
+     */
     public Academy(AdministratorId administratorId, Ruc ruc) {
         this.administratorId = administratorId;
         this.ruc = ruc;
     }
 
+    /**
+     * Constructs an Academy instance from a RegisterAcademyCommand.
+     *
+     * @param command the command containing the necessary data to register an academy
+     * @see RegisterAcademyCommand
+     */
     public Academy(RegisterAcademyCommand command) {
         this(command.administratorId(), command.ruc());
     }
