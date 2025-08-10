57a12f6cdf41a4ace7942ee371e0b417ce8dec8d
Salim Ramirez <salimramirezm@gmail.com>
2025-08-10 04:17:10 -0500

    docs(institution): enhance AcademyCommandServiceImpl with detailed Javadoc comments

--- src/main/java/com/demy/platform/institution/application/internal/commandservices/AcademyCommandServiceImpl.java
+++ src/main/java/com/demy/platform/institution/application/internal/commandservices/AcademyCommandServiceImpl.java
@@ -6,15 +6,40 @@ import com.demy.platform.institution.domain.services.AcademyCommandService;
 import com.demy.platform.institution.infrastructure.persistence.jpa.repositories.AcademyRepository;
 import org.springframework.stereotype.Service;
 
+/**
+ * Implementation of the {@link AcademyCommandService} interface for handling academy registration commands.
+ * <p>
+ * This service is responsible for processing commands related to academy registration,
+ * including validation and persistence of the academy entity.
+ *
+ * @author Salim Ramirez
+ * @see AcademyCommandService
+ * @see RegisterAcademyCommand
+ * @since 1.0.0
+ */
 @Service
 public class AcademyCommandServiceImpl implements AcademyCommandService {
 
     private final AcademyRepository academyRepository;
 
+    /**
+     * Constructs an instance of the class.
+     *
+     * @param academyRepository the repository used for academy persistence operations
+     */
     public AcademyCommandServiceImpl(AcademyRepository academyRepository) {
         this.academyRepository = academyRepository;
     }
 
+    /**
+     * {@inheritDoc}
+     * <p>
+     * This method processes the provided command to create a new academy
+     *
+     * @param command the command containing the necessary data to register an academy
+     * @return the identifier of the newly registered academy
+     * @see RegisterAcademyCommand
+     */
     @Override
     public Long handle(RegisterAcademyCommand command) {
         if (academyRepository.existsByRuc(command.ruc()))
