b0d48b3fe5bb807aaef22a804f6731a37495e15e
Salim Ramirez <salimramirezm@gmail.com>
2025-08-10 20:50:01 -0500

    feat(institution): add email, phone, and DNI number attributes to Administrator

--- src/main/java/com/demy/platform/institution/domain/model/aggregates/Administrator.java
+++ src/main/java/com/demy/platform/institution/domain/model/aggregates/Administrator.java
@@ -1,8 +1,7 @@
 package com.demy.platform.institution.domain.model.aggregates;
 
 import com.demy.platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
-import com.demy.platform.shared.domain.model.valueobjects.AcademyId;
-import com.demy.platform.shared.domain.model.valueobjects.FullName;
+import com.demy.platform.shared.domain.model.valueobjects.*;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import lombok.Getter;
@@ -13,6 +12,18 @@ public class Administrator extends AuditableAbstractAggregateRoot<Administrator>
     @Embedded
     @Getter
     private FullName fullName;
+
+    @Embedded
+    @Getter
+    private EmailAddress emailAddress;
+
+    @Embedded
+    @Getter
+    private PhoneNumber phoneNumber;
+
+    @Embedded
+    @Getter
+    private DniNumber dniNumber;
 
     @Embedded
     @Getter
