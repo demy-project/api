96f62fc4d6b6f65d717a30d3cb06c0031985e602
Salim Ramirez <salimramirezm@gmail.com>
2025-08-11 18:13:20 -0500

    feat(institution): add existsByDniNumber method to AdministratorRepository

--- src/main/java/com/demy/platform/institution/infrastructure/persistence/jpa/repositories/AdministratorRepository.java
+++ src/main/java/com/demy/platform/institution/infrastructure/persistence/jpa/repositories/AdministratorRepository.java
@@ -1,9 +1,12 @@
 package com.demy.platform.institution.infrastructure.persistence.jpa.repositories;
 
 import com.demy.platform.institution.domain.model.aggregates.Administrator;
+import com.demy.platform.shared.domain.model.valueobjects.DniNumber;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AdministratorRepository extends JpaRepository<Administrator, Long> {
+
+    boolean existsByDniNumber(DniNumber dniNumber);
 }
