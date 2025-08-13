1c0f15434f664e378c80120e59bc9dde754613e5
Salim Ramirez <salimramirezm@gmail.com>
2025-08-13 02:17:03 -0500

    feat(iam): add UserCommandService interface for handling user commands

---
+++ src/main/java/com/demy/platform/iam/domain/services/UserCommandService.java
@@ -0,0 +1,12 @@
+package com.demy.platform.iam.domain.services;
+
+import com.demy.platform.iam.domain.model.aggregates.User;
+import com.demy.platform.iam.domain.model.commands.SignInCommand;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+
+import java.util.Optional;
+
+public interface UserCommandService {
+
+    Optional<ImmutablePair<User, String>> handle(SignInCommand command);
+}
