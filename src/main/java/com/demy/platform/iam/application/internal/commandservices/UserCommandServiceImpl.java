4e9b6118ea4bb9e3bd836d70a61588e87fb7076e
Salim Ramirez <salimramirezm@gmail.com>
2025-08-13 15:19:01 -0500

    feat(iam): implement UserCommandService for handling user sign-in and sign-up

---
+++ src/main/java/com/demy/platform/iam/application/internal/commandservices/UserCommandServiceImpl.java
@@ -0,0 +1,52 @@
+package com.demy.platform.iam.application.internal.commandservices;
+
+import com.demy.platform.iam.application.internal.outboundservices.hashing.HashingService;
+import com.demy.platform.iam.application.internal.outboundservices.tokens.TokenService;
+import com.demy.platform.iam.domain.model.aggregates.User;
+import com.demy.platform.iam.domain.model.commands.SignInCommand;
+import com.demy.platform.iam.domain.model.commands.SignUpCommand;
+import com.demy.platform.iam.domain.services.UserCommandService;
+import com.demy.platform.iam.infrastructure.persistence.jpa.repositories.RoleRepository;
+import com.demy.platform.iam.infrastructure.persistence.jpa.repositories.UserRepository;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserCommandServiceImpl implements UserCommandService {
+
+    private final UserRepository userRepository;
+    private final HashingService hashingService;
+    private final TokenService tokenService;
+
+    private final RoleRepository roleRepository;
+
+    public UserCommandServiceImpl(UserRepository userRepository, HashingService hashingService, TokenService tokenService, RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.hashingService = hashingService;
+        this.tokenService = tokenService;
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public Optional<ImmutablePair<User, String>> handle(SignInCommand command) {
+        var user = userRepository.findByEmailAddress(command.emailAddress());
+        if (user.isEmpty())
+            throw new RuntimeException("User not found");
+        if (!hashingService.matches(command.password(), user.get().getPassword()))
+            throw new RuntimeException("Invalid password");
+        var token = tokenService.generateToken(user.get().getEmailAddress().email());
+        return Optional.of(ImmutablePair.of(user.get(), token));
+    }
+
+    @Override
+    public Optional<User> handle(SignUpCommand command) {
+        if (userRepository.existsByEmailAddress(command.emailAddress()))
+            throw new RuntimeException("Username already exists");
+        var roles = command.roles().stream().map(role -> roleRepository.findByName(role.getName()).orElseThrow(() -> new RuntimeException("Role name not found"))).toList();
+        var user = new User(command.emailAddress(), hashingService.encode(command.password()), roles);
+        userRepository.save(user);
+        return userRepository.findByEmailAddress(command.emailAddress());
+    }
+}
