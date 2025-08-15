8c9e58b740f66833653b4a543357a03081e9b3cf
Salim Ramirez <salimramirezm@gmail.com>
2025-08-15 03:40:51 -0500

    feat(iam): add TenantId field and association methods to User entity

--- src/main/java/com/demy/platform/iam/domain/model/aggregates/User.java
+++ src/main/java/com/demy/platform/iam/domain/model/aggregates/User.java
@@ -1,6 +1,7 @@
 package com.demy.platform.iam.domain.model.aggregates;
 
 import com.demy.platform.iam.domain.model.entities.Role;
+import com.demy.platform.iam.domain.model.valueobjects.TenantId;
 import com.demy.platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
 import com.demy.platform.shared.domain.model.valueobjects.EmailAddress;
 import jakarta.persistence.*;
@@ -27,9 +28,12 @@ public class User extends AuditableAbstractAggregateRoot<User> {
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_roles",
-            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
+
+    @Embedded
+    private TenantId tenantId;
 
     public User() {}
 
@@ -37,6 +41,7 @@ public class User extends AuditableAbstractAggregateRoot<User> {
         this.emailAddress = emailAddress;
         this.password = password;
         this.roles = new HashSet<>();
+        this.tenantId = new TenantId();
     }
 
     public User(EmailAddress emailAddress, String password, List<Role> roles) {
@@ -54,5 +59,25 @@ public class User extends AuditableAbstractAggregateRoot<User> {
         var validateRoleSet = Role.validateRoleSet(roles);
         this.roles.addAll(validateRoleSet);
         return this;
+    }
+
+    public Long getTenantIdOrNull() {
+        return (tenantId != null && tenantId.isAssigned()) ? tenantId.tenantId() : null;
+    }
+
+    public void associateTenant(TenantId tenantId) {
+        if (this.tenantId == null) {
+            this.tenantId = tenantId;
+        } else {
+            throw new IllegalStateException("User is already associated with a tenant");
+        }
+    }
+
+    public void disassociateTenant(TenantId tenantId) {
+        if (this.tenantId != null && this.tenantId.equals(tenantId)) {
+            this.tenantId = null;
+        } else {
+            throw new IllegalStateException("User is not associated with the specified tenant");
+        }
     }
 }
