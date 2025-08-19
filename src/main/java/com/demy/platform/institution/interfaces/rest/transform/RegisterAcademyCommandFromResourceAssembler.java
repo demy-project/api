29f0fee80d2104d07ffb79c8f8fb338472c3aec7
Salim Ramirez <salimramirezm@gmail.com>
2025-08-19 18:06:03 -0500

    feat(institution): remove AdministratorId from RegisterAcademyCommand conversion

--- src/main/java/com/demy/platform/institution/interfaces/rest/transform/RegisterAcademyCommandFromResourceAssembler.java
+++ src/main/java/com/demy/platform/institution/interfaces/rest/transform/RegisterAcademyCommandFromResourceAssembler.java
@@ -3,7 +3,6 @@ package com.demy.platform.institution.interfaces.rest.transform;
 import com.demy.platform.institution.domain.model.commands.RegisterAcademyCommand;
 import com.demy.platform.institution.domain.model.valueobjects.AcademyDescription;
 import com.demy.platform.institution.domain.model.valueobjects.AcademyName;
-import com.demy.platform.institution.domain.model.valueobjects.AdministratorId;
 import com.demy.platform.institution.domain.model.valueobjects.Ruc;
 import com.demy.platform.institution.interfaces.rest.resources.RegisterAcademyResource;
 import com.demy.platform.shared.domain.model.valueobjects.EmailAddress;
@@ -13,7 +12,6 @@ import com.demy.platform.shared.domain.model.valueobjects.StreetAddress;
 public class RegisterAcademyCommandFromResourceAssembler {
     public static RegisterAcademyCommand toCommandFromResource(RegisterAcademyResource resource) {
         return new RegisterAcademyCommand(
-                new AdministratorId(resource.administratorId()),
                 new AcademyName(resource.academyName()),
                 new AcademyDescription(resource.academyDescription()),
                 new StreetAddress(
