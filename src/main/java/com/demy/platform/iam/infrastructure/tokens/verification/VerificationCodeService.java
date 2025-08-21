871105d515b0dc7b745ddd831c909c58041696de
Salim Ramirez <salimramirezm@gmail.com>
2025-08-21 18:11:18 -0500

    feat(iam): add VerificationCodeService interface extending CodeService

---
+++ src/main/java/com/demy/platform/iam/infrastructure/tokens/verification/VerificationCodeService.java
@@ -0,0 +1,6 @@
+package com.demy.platform.iam.infrastructure.tokens.verification;
+
+import com.demy.platform.iam.application.internal.outboundservices.tokens.CodeService;
+
+public interface VerificationCodeService extends CodeService {
+}
