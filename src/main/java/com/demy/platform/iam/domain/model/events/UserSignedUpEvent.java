51e85e533148eb27fa48a543b898250bebafc435
Salim Ramirez <salimramirezm@gmail.com>
2025-08-21 15:11:00 -0500

    feat(iam): update UserSignedUpEvent to use User object instead of userId

--- src/main/java/com/demy/platform/iam/domain/model/events/UserSignedUpEvent.java
+++ src/main/java/com/demy/platform/iam/domain/model/events/UserSignedUpEvent.java
@@ -1,5 +1,6 @@
 package com.demy.platform.iam.domain.model.events;
 
+import com.demy.platform.iam.domain.model.aggregates.User;
 import com.demy.platform.iam.domain.model.entities.Role;
 import lombok.Getter;
 import org.springframework.context.ApplicationEvent;
@@ -9,12 +10,12 @@ import java.util.List;
 @Getter
 public class UserSignedUpEvent extends ApplicationEvent {
 
-    private final Long userId;
+    private final User user;
     private final List<Role> roles;
 
-    public UserSignedUpEvent(Object source, Long userId, List<Role> roles) {
+    public UserSignedUpEvent(Object source, User user, List<Role> roles) {
         super(source);
-        this.userId = userId;
+        this.user = user;
         this.roles = roles;
     }
 }
