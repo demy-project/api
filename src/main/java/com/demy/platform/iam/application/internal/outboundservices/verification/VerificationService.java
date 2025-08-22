5fc98689c1c5a3570390ad589bfe1b0d74817c62
Salim Ramirez <salimramirezm@gmail.com>
2025-08-22 03:35:26 -0500

    feat(iam): update VerificationService to return string from generateCode and add generateExpirationMinutes method

--- src/main/java/com/demy/platform/iam/application/internal/outboundservices/verification/VerificationService.java
+++ src/main/java/com/demy/platform/iam/application/internal/outboundservices/verification/VerificationService.java
@@ -1,14 +1,14 @@
 package com.demy.platform.iam.application.internal.outboundservices.verification;
 
-import com.demy.platform.iam.domain.model.valueobjects.VerificationCode;
-
 import java.time.LocalDateTime;
 
 public interface VerificationService {
 
-    VerificationCode generateCode();
+    String generateCode();
 
     String generateCode(int length);
+
+    Integer generateExpirationMinutes();
 
     boolean verifyCode(String code, String expectedCode, LocalDateTime expirationTime);
 }
