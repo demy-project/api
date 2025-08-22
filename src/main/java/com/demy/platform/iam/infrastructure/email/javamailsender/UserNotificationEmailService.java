ba1a7b9cfc7d6bcb98ff80af56a9bdb8411db410
Salim Ramirez <salimramirezm@gmail.com>
2025-08-22 00:42:26 -0500

    feat(iam): update UserNotificationEmailService to extend NotificationEmailService

--- src/main/java/com/demy/platform/iam/infrastructure/email/javamailsender/UserNotificationEmailService.java
+++ src/main/java/com/demy/platform/iam/infrastructure/email/javamailsender/UserNotificationEmailService.java
@@ -1,6 +1,7 @@
 package com.demy.platform.iam.infrastructure.email.javamailsender;
 
-import com.demy.platform.iam.application.internal.outboundservices.email.EmailService;
+import com.demy.platform.iam.application.internal.outboundservices.email.NotificationEmailService;
 
-public interface UserNotificationEmailService extends EmailService {
+public interface UserNotificationEmailService extends NotificationEmailService {
+
 }
