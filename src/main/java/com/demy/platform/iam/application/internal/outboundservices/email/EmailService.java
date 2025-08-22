49bd7f39ceabb316a7b1c0b512f08add4cd9b9c8
Salim Ramirez <salimramirezm@gmail.com>
2025-08-22 00:35:32 -0500

    feat(iam): add NotificationEmailService interface for sending verification emails

--- src/main/java/com/demy/platform/iam/application/internal/outboundservices/email/EmailService.java
+++
@@ -1,8 +0,0 @@
-package com.demy.platform.iam.application.internal.outboundservices.email;
-
-import java.util.Map;
-
-public interface EmailService {
-
-    void sendEmail(String to, String subject, String templateName, Map<String, Object> variables);
-}
---
+++ src/main/java/com/demy/platform/iam/application/internal/outboundservices/email/NotificationEmailService.java
@@ -0,0 +1,5 @@
+package com.demy.platform.iam.application.internal.outboundservices.email;
+
+public interface NotificationEmailService {
+    void sendVerificationEmail(String to, String token, int expirationMinutes);
+}
