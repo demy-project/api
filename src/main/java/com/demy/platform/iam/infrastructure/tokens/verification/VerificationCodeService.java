ed5243a529c38cc4f6e1e768b596d217706d804a
Salim Ramirez <salimramirezm@gmail.com>
2025-08-22 01:52:03 -0500

    feat(iam): rename VerificationCodeService to OtpSecureVerificationService and update package structure

--- src/main/java/com/demy/platform/iam/infrastructure/tokens/verification/VerificationCodeService.java
+++
@@ -1,6 +0,0 @@
-package com.demy.platform.iam.infrastructure.tokens.verification;
-
-import com.demy.platform.iam.application.internal.outboundservices.verification.VerificationService;
-
-public interface VerificationCodeService extends VerificationService {
-}
