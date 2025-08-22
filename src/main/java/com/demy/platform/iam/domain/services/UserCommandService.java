9c47bb086d61773f8c6e8dbf918adf029d41b2cd
Salim Ramirez <salimramirezm@gmail.com>
2025-08-22 03:34:45 -0500

    feat(iam): add command handlers for user verification and code resending

--- src/main/java/com/demy/platform/iam/domain/services/UserCommandService.java
+++ src/main/java/com/demy/platform/iam/domain/services/UserCommandService.java
@@ -1,9 +1,7 @@
 package com.demy.platform.iam.domain.services;
 
 import com.demy.platform.iam.domain.model.aggregates.User;
-import com.demy.platform.iam.domain.model.commands.AssignUserTenantId;
-import com.demy.platform.iam.domain.model.commands.SignInCommand;
-import com.demy.platform.iam.domain.model.commands.SignUpCommand;
+import com.demy.platform.iam.domain.model.commands.*;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 
 import java.util.Optional;
@@ -13,6 +11,10 @@ public interface UserCommandService {
     Optional<ImmutablePair<User, String>> handle(SignInCommand command);
 
     Optional<User> handle(SignUpCommand command);
+
+    boolean handle(VerifyUserCommand command);
+
+    boolean handle(ResendVerificationCodeCommand command);
 
     void handle(AssignUserTenantId command);
 }
