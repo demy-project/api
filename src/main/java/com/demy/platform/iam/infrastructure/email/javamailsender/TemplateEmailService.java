5ed9f6f2f27f050b2d581fdaefe498e0aded0f24
Salim Ramirez <salimramirezm@gmail.com>
2025-08-21 20:32:49 -0500

    refactor(shared): rename TemplateEmailService to TemplatedEmailService and update package structure

--- src/main/java/com/demy/platform/iam/infrastructure/email/javamailsender/TemplateEmailService.java
+++
@@ -1,7 +0,0 @@
-package com.demy.platform.iam.infrastructure.email.javamailsender;
-
-import com.demy.platform.iam.application.internal.outboundservices.email.EmailService;
-
-public interface TemplateEmailService extends EmailService {
-
-}
---
+++ src/main/java/com/demy/platform/shared/infrastructure/email/javamailsender/TemplatedEmailService.java
@@ -0,0 +1,7 @@
+package com.demy.platform.shared.infrastructure.email.javamailsender;
+
+import com.demy.platform.iam.application.internal.outboundservices.email.EmailService;
+
+public interface TemplatedEmailService extends EmailService {
+
+}
