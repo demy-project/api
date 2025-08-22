8a61ed11a4cbc4683387ca2acf229d5c419b4c18
Salim Ramirez <salimramirezm@gmail.com>
2025-08-22 00:35:57 -0500

    feat(iam): add sendPasswordResetEmail method to NotificationEmailService

--- src/main/java/com/demy/platform/iam/application/internal/outboundservices/email/NotificationEmailService.java
+++ src/main/java/com/demy/platform/iam/application/internal/outboundservices/email/NotificationEmailService.java
@@ -1,5 +1,8 @@
 package com.demy.platform.iam.application.internal.outboundservices.email;
 
 public interface NotificationEmailService {
+
     void sendVerificationEmail(String to, String token, int expirationMinutes);
+
+    void sendPasswordResetEmail(String to, String resetLink);
 }
