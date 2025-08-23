de515549cf0220682e7778bf7629ab4ce2aaeb95
Salim Ramirez <salimramirezm@gmail.com>
2025-08-23 01:26:00 -0500

    feat(exception): add errorCode field and conversion method to DomainException

--- src/main/java/com/demy/platform/shared/domain/exceptions/DomainException.java
+++ src/main/java/com/demy/platform/shared/domain/exceptions/DomainException.java
@@ -5,10 +5,16 @@ import lombok.Getter;
 @Getter
 public abstract class DomainException extends RuntimeException {
     private final String messageCode;
+    private final String errorCode;
     private final Object[] args;
     protected DomainException(String messageCode, Object... args) {
         super(messageCode);
         this.messageCode = messageCode;
+        this.errorCode = toErrorCode(messageCode);
         this.args = args;
+    }
+
+    private String toErrorCode(String messageCode) {
+        return messageCode.toUpperCase().replace('.', '_');
     }
 }
