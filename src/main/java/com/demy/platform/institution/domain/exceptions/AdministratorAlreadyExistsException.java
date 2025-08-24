af3e6b3b80ee109f475074ceecb04b6ed8a3a64a
Salim Ramirez <salimramirezm@gmail.com>
2025-08-24 04:09:51 -0500

    feat(institution): update AdministratorAlreadyExistsException to use InstitutionError for improved consistency

--- src/main/java/com/demy/platform/institution/domain/exceptions/AdministratorAlreadyExistsException.java
+++ src/main/java/com/demy/platform/institution/domain/exceptions/AdministratorAlreadyExistsException.java
@@ -1,10 +1,11 @@
 package com.demy.platform.institution.domain.exceptions;
 
+import com.demy.platform.institution.domain.errors.InstitutionError;
 import com.demy.platform.shared.domain.exceptions.DomainException;
 import com.demy.platform.shared.domain.model.valueobjects.DniNumber;
 
 public class AdministratorAlreadyExistsException extends DomainException {
     public AdministratorAlreadyExistsException(DniNumber dniNumber) {
-        super("institution.administrator.already.exists", dniNumber.dniNumber());
+        super(InstitutionError.ADMINISTRATOR_ALREADY_EXISTS, dniNumber.dniNumber());
     }
 }
