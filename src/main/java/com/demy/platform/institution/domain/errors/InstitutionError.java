ce54c5930a245342401576a66aedfbf6f1f7be79
Salim Ramirez <salimramirezm@gmail.com>
2025-08-24 04:48:32 -0500

    feat(institution): add InstitutionError enum for consistent error handling

---
+++ src/main/java/com/demy/platform/institution/domain/errors/InstitutionError.java
@@ -0,0 +1,7 @@
+package com.demy.platform.institution.domain.errors;
+
+import com.demy.platform.shared.domain.errors.DomainError;
+
+public enum InstitutionError implements DomainError {
+    ADMINISTRATOR_ALREADY_EXISTS;
+}
